96325fe4c7d49832d7858271006c0cea37da0a56
NYR <paphat.dev@gmail.com>
2015-12-26 15:01:29 +0700

    Finish Day1

--- app/src/main/java/com/pw/paphat/mytraffic/MainActivity.java
+++ app/src/main/java/com/pw/paphat/mytraffic/MainActivity.java
@@ -1,13 +1,51 @@
 package com.pw.paphat.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    }   //Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cat);
+                buttonMediaPlayer.start();
+
+
+            }   //Event
+        });
+
+    }   //buttonController
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.buttonok);
+
     }
 }
